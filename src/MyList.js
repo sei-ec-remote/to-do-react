bae9714d9f71e9ae3b7e2a61a632ca02e0d0dfc1
lyee197 <liamsyee@gmail.com>
2022-03-23 16:57:56 -0400

    Added ListItem to MyList

---
+++ src/MyList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>
+          <ListItem />
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
