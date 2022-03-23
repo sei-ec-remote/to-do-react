674ab0edb235fa934b7e76a15549a61c2a63bfe1
cloud3999 <edwardlcloud@gmail.com>
2022-03-23 17:15:17 -0400

    initial App Setup as per Deliverable instructions

---
+++ src/MyList.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import React, { Component } from 'react'
+import './App.css';
+import ListItem from './ListItem';
+
+class MyList extends Component {
+  render() {
+
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
+
+    return (
+      <div class="toDo">
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList;
