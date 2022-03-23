fd99e97ee80406b4a6747eea016c1aeb8f3312ed
CameronASC20 <camerongarnier73@gmail.com>
2022-03-23 19:06:40 -0400

    Homework done

---
+++ src/ListItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class ListItem extends Component {
+  render() {
+    return(
+        <>
+            <li>{this.props.task}</li>
+        </>
+    )
+  }
+}
+
+export default ListItem
