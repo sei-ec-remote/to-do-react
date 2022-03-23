5acf281e8358ebce25c8718f4165a98727a2bf78
kaiguy920 <smithkai920@gmail.com>
2022-03-23 16:48:56 -0400

    connected to React & have nested items in to-do list

---
+++ src/MyList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>
+          <ListItem task="Buy ice cream" />
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
