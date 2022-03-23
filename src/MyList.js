d027b1eacaf24b973f6709f545c230cf8ad2d87a
maiky5198 <maiky5198@gmail.com>
2022-03-23 19:11:05 -0400

    hw

---
+++ src/MyList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+
+// const phrases = ['ice cream', 'dinosaurs', 'hobbits']
+// let excitedPhrases = phrases.map( (phrase, index) => {
+//   return newPhrase = phrase + '!'
+// })
+
+class MyList extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
+    
+    return (
+      <div>
+        <h1>Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
