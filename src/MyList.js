dbc7e9594d7f8731a361e3455d9e9360966f1f5e
STEVIEMILITELLO <steviemilitello@gmail.com>
2022-03-24 09:47:10 -0400

    adds form

--- src/MyList.js
+++ src/MyList.js
@@ -11,13 +11,17 @@ class MyList extends Component {
     let todoItems = this.props.theList.map((item, index) => {
       return <ListItem task={item} key={index} />
     })
-    
     return (
       <div>
         <h1>Things I should stop procrastinating:</h1>
         <ul>
           {todoItems}
         </ul>
+          <form> 
+            <input type="text" />
+            <input type="submit" value="Add it!" />
+            <input type="submit" value="Finished the list!"/>
+          </form>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ const toDos = ["Listen to Hot Mulligan", "Drink hot cocoa", "Eat hot cheetos"]
 
 ReactDOM.render(
   <React.StrictMode>
-  <MyList theList={toDos} />,
+  <MyList theList={toDos} />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
