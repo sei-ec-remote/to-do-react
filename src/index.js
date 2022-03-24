f7fc60b34408e115c1bc78732aad6685ec894653
ldomino <domino.laurie@gmail.com>
2022-03-23 19:55:51 -0500

    Created MyList, Added display MyList, Added array toDos and display

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import reportWebVitals from './reportWebVitals';
+
+const toDos = ["Read a great book", "Drink some Almond Milk", "Follow a Recipe"]
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyList theList={toDos} />
   </React.StrictMode>,
   document.getElementById('root')
 );
