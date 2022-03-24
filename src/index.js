b611353cc55d7e4cd11877f2aded5cd9e6cf95fb
aoehmann239 <aoehmann239@gmail.com>
2022-03-23 23:06:31 -0400

    Deliverable complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import reportWebVitals from './reportWebVitals';
+
+const toDos = ['Laundry', 'Clean the dishes', 'Get gas'];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyList theList={toDos} />
   </React.StrictMode>,
   document.getElementById('root')
 );
