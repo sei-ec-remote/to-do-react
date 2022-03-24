34e2d414b5fe5564886d95bc9bf2913d97e840a9
Justin Waddle <justinfrankwaddle@gmail.com>
2022-03-23 22:08:31 -0500

    finished to-do react deliverable

---
+++ src/AddThing.js
@@ -0,0 +1,21 @@
+// import React, { Component, useState } from 'react'
+// import './App.css'
+
+
+// class AddThing extends Component {
+//   render() {
+//     function addsThing () {
+//         const thing = document.getElementById('thingToAdd').value
+        
+//     }
+//     return (
+//         <>
+//             <input id='thingToAdd' type='text' placeholder='Enter Thing Here'></input>
+//             <button value='send' onClick={addsThing()}>Add it!</button>
+//         </>
+
+//     )
+//   }
+// }
+
+// export default AddThing
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
+//import AddThing from './AddThing';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const toDos = ["Listen to Hot Mulligan", "Drink hot cocoa", "Eat hot cheetos","learning react","learning ds&algo"]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos} />,
+  // <AddThing/>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
