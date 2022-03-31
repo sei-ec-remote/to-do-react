dd4ec1ceb484197113c96c987113e12eb0e16760
Nash4star <forrester.nash@gmail.com>
2022-03-31 05:13:29 -0400

    completed to-do-list

---
+++ src/MyList.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ListItem from './ListItem'
+import './App.css'
+
+class Mylist extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
+
+    return (
+      <div>
+        <h1> Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Mylist
