72c3d8de707cf7bd6a7f6d2b9b3fb01ca99033b3
danstacy1 <danstacy1@gmail.com>
2022-07-22 20:03:14 -0400

    connection est, made MyList and ListItem, and created CSS.

---
+++ src/MyList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './MyList.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    return(<div>
+      <h1>Things I should stop procrastinating:</h1>
+      <ul>
+        <ListItem />
+      </ul>
+    </div>)
+  }
+}
+
+export default MyList
