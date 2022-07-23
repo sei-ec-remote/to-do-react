078cfcc28028ac4957c5e8d5d40ac85c84a2c270
Alys <alice.g.cooper@gmail.com>
2022-07-23 10:23:03 -0400

    Set up todo list

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import MyList from './MyList';
 import './index.css';
 
+const toDos = ["Go to a Fratellis Concert", "Drink 8 cups of water a day", "Take the dogs on a walk"]
+
 ReactDOM.render(
-  <MyList />,
+  <MyList theList={toDos} />,
   document.getElementById('root')
 )
