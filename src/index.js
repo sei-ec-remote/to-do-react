eb74e370a95fe7182f0b2463438e06d831c5427c
jlx012 <james.lee01230@gmail.com>
2022-07-24 18:59:18 -0500

    Completed Deliverable

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import reportWebVitals from './reportWebVitals';
+const toDos = ['Buy ice cream', 'Eat dinosaurs', 'Fight hobbits']
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyList theList={toDos}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
