e0e94f0abc09b6efba949aef51bb643e2c9ad116
Ryan May <94943821+mayday91@users.noreply.github.com>
2022-07-31 16:18:06 -0400

    finished deliverable

---
+++ src/MyList.js
@@ -0,0 +1,45 @@
+
+// function MyList () {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React, { Component } from 'react'
+import './App.css';
+import ListItem from './ListItem'
+
+
+class MyList extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
+    return (
+      <div>
+        <h1> Things I should stop procrastinating: </h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+      )
+  }
+}
+
+
+export default MyList;
