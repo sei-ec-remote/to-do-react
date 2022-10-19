db669f467a070486ff9c10300d8a207b3e26696f
lozier <latayroutier@gmail.com>
2022-10-19 16:11:58 -0400

    added styling and ListItem

---
+++ src/ListItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class ListItem extends Component {
+    render() {
+        return(<li>Make the List!</li>)
+    }
+}
+
+export default ListItem
