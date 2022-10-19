6207a965f4fd2a5ed415adc25a468eff69d6c606
lozier <latayroutier@gmail.com>
2022-10-19 15:57:49 -0400

    connected to server, changed app to My List

---
+++ src/MyList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class MyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Things I should stop procrastinating: </h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
