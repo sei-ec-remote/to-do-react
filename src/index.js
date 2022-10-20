272b484e74f7020cd52000a3faf7e3568d8bf4e3
JChenoweth <jchenoweth@Jennifers-MBP.local>
2022-10-20 08:03:40 -0500

    completed deliverable

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const toDos = ["Buy ice cream", "Eat ice cream", "Go to the gym"]
+
+const submitBtn = document.getElementById('submit')
+const inputItem = document.getElementById('text').value
+
+submitBtn.addEventListener('click', () => {
+  toDos.push(inputItem)
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
