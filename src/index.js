275417de53f6c7f18b3b1850bd4aea59e6717a18
saparker <stephanieparker1997@gmail.com>
2022-10-21 09:41:34 -0400

    Stephanie Parker Completed HW

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import reportWebVitals from './reportWebVitals';
-
+const toDos = ['cat-litter', 'take out the trash', 'laundry', 'plan for world domination']
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyList theList = {toDos}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
