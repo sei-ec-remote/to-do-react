20dbd784571af906cd4874a1d8e7eb26e3fe8f13
Travis <travishersh0@gmail.com>
2023-02-02 15:55:38 -0500

    Added todos and search bar with buttons but no funtionality

--- src/MyList.js
+++ src/MyList.js
@@ -4,12 +4,22 @@ import ListItem from './components/ListItem'
 
 class MyList extends Component {
   render() {
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
     return (
       <div>
         <h1>Things I should stop procrastinating:</h1>
         <ul>
-          <ListItem task="Buy ice cream"/>
+          {todoItems}
         </ul>
+        <div>
+        <input
+        type="text"
+        placeholder="Type an item here" />
+        <button>Add it!</button>
+        </div>
+        <button className='button'>Finished the list!</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import MyList from './MyList';
 import './index.css';
 
+const toDos = [
+  "Listen to Hot Mulligan", 
+  "Drink hot cocoa", 
+  "Eat hot cheetos"
+]
+
 ReactDOM.render(
-  <MyList />,
+  <MyList theList={toDos}/>,
   document.getElementById('root')
 )
