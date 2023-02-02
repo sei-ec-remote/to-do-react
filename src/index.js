d7bca0a3eb8b355e6a99ab2317fe45f39c4fcd08
EricCSheppard <EricCSheppard@gmail.com>
2023-02-02 16:54:22 -0500

    finished deliverable

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyList from './MyList';
 import reportWebVitals from './reportWebVitals';
+
+const toDos = ['Homework', 'More Homework', 'Even More Homework!']
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyList theList={toDos}/>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
